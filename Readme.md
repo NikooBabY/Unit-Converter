9e83b8c8122450138db4a10b22a52aad0610b9af
Sulav Karki <karkisulav31@gmail.com>
2022-02-25 11:45:51 +0545

    Inital and final commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Basic Unit Converter
+A unit converter that converts Nepali land measurement system units.
+
+
+# Usage
+1. Git clone the repository
+2. Type ``py "Unit Converter.py"`` into the terminal
--- Unit converter.py
+++ Unit converter.py
@@ -1,6 +1,5 @@
 from tkinter import *
 import tkinter.font as font
-from functools import partial
 from tkinter import messagebox
 from tkinter import ttk
 
@@ -14,6 +13,29 @@ root.configure(bg='Sky blue')
 f1=font.Font(family='Serif',size='30')
 f2=font.Font(family='Serif',size='10')
 f3=font.Font(family='Serif',size='20')
+
+values_dict = {
+        "Dhur" : {"Dhur" : 1, "Kattha" : 0.05, "Bigha" : 0.0025, "Ropani" : 0.033275, "Aana" : 0.5324, "Paisa" : 2.1296},
+        "Kattha" : {"Dhur" : 20, "Kattha" : 1, "Bigha" : 0.05, "Ropani" : 0.6655, "Aana" : 10.648, "Paisa" : 42.592},
+        "Bigha" : {"Dhur" : 400, "Kattha" : 20, "Bigha" : 1, "Ropani" : 13.31, "Aana" : 212.96, "Paisa" : 851.84},
+        "Ropani" : {"Dhur" : 30.05259, "Kattha" : 1.50262, "Bigha" : 0.07513, "Ropani" : 1, "Aana" : 16, "Paisa" : 64},
+        "Aana" : {"Dhur" : 1087828, "Kattha" : 0.09391, "Bigha" : 0.00469, "Ropani" : 0.0625, "Aana" : 1, "Paisa" : 4},
+        "Paisa" : { "Dhur" : 0.46957, "Kattha" : 0.02347, "Bigha" : 0.001173, "Ropani" : 0.015625, "Aana" : 0.25, "Paisa" : 1},
+                    }
+
+def cver_value(var1, var2, values_dict=values_dict):
+    return values_dict[var1].get(var2)
+
+def answer(var1, var2, num):
+    try:
+        num = int(num)
+        if cver_value(var1, var2) == None:
+            messagebox.showerror('Error',"Use valid terms.")
+        else:
+            result.cget("text")
+            result.configure(text=((num * cver_value(var1, var2), var2)))
+    except:
+        messagebox.showerror()
 
 def selected(event):
     global unit
@@ -33,187 +55,6 @@ def tofunc(event):
     result_to=event.widget.get()
 
 
-def answer(n1):
-    num1=n1.get()
-    try:
-        number1 = int(num1)
-    except:
-        messagebox.showerror('Error',"Use valid terms.")
-
-    if result_from == 'Dhur' and result_to == 'Dhur':
-        calculate = number1
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Dhur' and result_to == 'Kattha':
-        calculate = number1*0.05
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Dhur' and result_to == 'Bigha':
-        calculate = number1*0.0025
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Dhur' and result_to == 'Ropani':
-        calculate = number1*0.033275
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Dhur' and result_to == 'Aana':
-        calculate = number1*0.5324
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Dhur' and result_to == 'Paisa':
-        calculate = number1*2.1296
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Kattha' and result_to == 'Kattha':
-        calculate = number1
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Kattha' and result_to == 'Dhur':
-        calculate = number1*20
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Kattha' and result_to == 'Bigha':
-        calculate = number1*0.05
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Kattha' and result_to == 'Ropani':
-        calculate = number1*0.6655
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Kattha' and result_to == 'Aana':
-        calculate = number1*10.648
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Kattha' and result_to == 'Paisa':
-        calculate = number1*42.592
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Bigha' and result_to == 'Bigha':
-        calculate = number1
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Bigha' and result_to == 'Dhur':
-        calculate = number1*400
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Bigha' and result_to == 'Kattha':
-        calculate = number1*20
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Bigha' and result_to == 'Ropani':
-        calculate = number1*13.31
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Bigha' and result_to == 'Aana':
-        calculate = number1*212.96
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Bigha' and result_to == 'Paisa':
-        calculate = number1*851.84
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Ropani' and result_to == 'Ropani':
-        calculate = number1
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Ropani' and result_to == 'Dhur':
-        calculate = number1*30.05259
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Ropani' and result_to == 'Kattha':
-        calculate = number1*1.50262
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Ropani' and result_to == 'Bigha':
-        calculate = number1*0.07513
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Ropani' and result_to == 'Aana':
-        calculate = number1*16
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Ropani' and result_to == 'Paisa':
-        calculate = number1*64
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Aana' and result_to == 'Aana':
-        calculate = number1
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Aana' and result_to == 'Dhur':
-        calculate = number1*1.87828
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Aana' and result_to == 'Bigha':
-        calculate = number1*0.00469
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Aana' and result_to == 'Ropani':
-        calculate = number1*0.0625
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Aana' and result_to == 'Paisa':
-        calculate = number1*4
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Paisa' and result_to == 'Paisa':
-        calculate = number1
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Paisa' and result_to == 'Dhur':
-        calculate = number1*0.46957
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Paisa' and result_to == 'Kattha':
-        calculate = number1*0.02347
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Paisa' and result_to == 'Bigha':
-        calculate = number1*0.001173
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Paisa' and result_to == 'Ropani':
-        calculate = number1*0.015625
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
-
-    elif result_from == 'Paisa' and result_to == 'Aana':
-        calculate = number1*0.25
-        result.cget('text')
-        result.configure(text=(calculate,result_to))
 
 main =Label(root,text='Unit Converter', bg='Sky blue',fg='Red')
 main['font']=f1
@@ -234,37 +75,30 @@ to.place(relx='0.268', rely='0.45')
 unitdd=ttk.Combobox(root,width='35',)
 unitdd['values']=('LandAreaMeasurement')
 unitdd.place(relx='0.57',rely=0.3,anchor='center')
-#unitdd.current()
 unitdd.bind('<<ComboboxSelected>>' ,selected)
 
 
 f=StringVar()
 fromdd= ttk.Combobox(root,width='35',textvariable=f)
 fromdd.place(relx='0.57',rely='0.37',anchor='center')
-#fromdd.current()
 fromdd.bind('<<ComboboxSelected>>',fromfunc)
 
 num=StringVar()
 num_from=Entry(root,width=10,textvariable= num)
 num_from.place(relx='0.9',rely='0.37',anchor='center')
 
-answer=partial(answer,num_from)
-
-
 
 t=StringVar()
 todd=ttk.Combobox(root,width='35',textvariable=t)
 todd.place(relx='0.57', rely='0.47', anchor='center')
-#todd.current()
 todd.bind('<<ComboboxSelected>>',tofunc)
 
 result=Label(root,text='',bg='white',width=20)
 result['font']=f3
 result.place(relx='0.21',rely='0.6')
 
-convert=Button(root,text='Convert',bg='#c8cfde',command=answer)
+convert=Button(root,text='Convert',bg='#c8cfde',command=lambda: answer(f.get(), t.get(), num.get()))
 convert['font']=f2
 convert.place(relx='0.46',rely='0.7')
-
 
 root.mainloop()
